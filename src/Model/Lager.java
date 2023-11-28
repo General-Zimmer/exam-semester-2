dea30b6a6252b3a6be7ac7e19b7a3fc5495d6fe3
GZimmer <kristianz1000@gmail.com>
2023-11-28 10:27:04 +0100

    Fixed Lager constructor parameter for fad Array

--- src/Model/Lager.java
+++ src/Model/Lager.java
@@ -6,9 +6,9 @@ public class Lager {
     private final UUID ID; // Et unikt ID, vi kan generere for at adskille hvert objekt
     private final Fad[][] reoler; // 2-dimensionelt reol-system. Med flere 2D reoler, har vi et 3D lager
 
-    public Lager(UUID ID, Fad[][] reoler) {
+    public Lager(UUID ID, int reoler, int reolKapacitet) {
         this.ID = ID;
-        this.reoler = reoler;
+        this.reoler = new Fad[reoler][reolKapacitet];
     }
 
     public UUID getID() {
