0358e3c704a9379b42e6756fb2e17073c5ebab32
nikolai <nikolaijuhl@gmail.com>
2023-11-30 09:55:58 +0100

    LagerTest Testcases

--- test/LagerTest.java
+++ test/LagerTest.java
@@ -1,4 +1,7 @@
 import controller.Controller;
+import model.Lager;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import storage.IStorage;
@@ -18,23 +21,46 @@ public class LagerTest {
 
 
     @Test
-    public void getAntalTommePladser() {
-
-
-
-        //arrange
-        //act
-        //assert
-    }
-
-
-    @Test
     public void redigerLager() {
 
-
+        // TestCase 1
 
         //arrange
+        Lager testLager = Controller.createLager("Sønderhøj 30, 8240 Viby", 2, 4);
+
         //act
+        testLager.redigerReoler(3, 5);
+
+
         //assert
+        for (int i = 0; i < testLager.getReoler().length; i++) {
+            Assertions.assertEquals(3, testLager.getReoler().length);
+
+            for (int j = 0; j < testLager.getReoler()[i].length; j++) {
+                Assertions.assertEquals(5, testLager.getReoler()[i].length);
+            }
+
+        }
+
+
+        // TestCase 2
+
+
+        // Arange
+        Lager testLager2 = Controller.createLager("Sønderhøj 30, 8240 Viby", 2, 4);
+
+        //act
+        testLager2.redigerReoler(1, 3);
+
+        // assert
+        for (int i = 0; i < testLager2.getReoler().length; i++) {
+            Assertions.assertEquals(1, testLager2.getReoler().length);
+
+            for (int j = 0; j < testLager2.getReoler()[i].length; j++) {
+                Assertions.assertEquals(3, testLager2.getReoler()[i].length);
+            }
+
+        }
+
     }
 }
