a83f8e18e7c6cd9cf43df6dd20bf8faf81afd23a
GZimmer <kristianz1000@gmail.com>
2023-11-30 10:00:31 +0100

    Forgot to subtract one from biggestSize

--- src/model/Lager.java
+++ src/model/Lager.java
@@ -46,7 +46,7 @@ public class Lager {
      */
     public void redigerReoler(int reoler, int reolKapacitet) {
         Fad[][] fadLagerTing = new Fad[reoler][reolKapacitet];
-        int biggestSize = Math.max(this.reoler[0].length, fadLagerTing[0].length);
+        int biggestSize = Math.max(this.reoler[0].length, fadLagerTing[0].length)-1;
 
         for (int i = 0; i < this.reoler.length; i++) {
             System.arraycopy(this.reoler[i], 0, fadLagerTing[i], 0, biggestSize);
