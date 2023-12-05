4d6fe562ee139a595df9c96749eac0d209c2e90f
GZimmer <kristianz1000@gmail.com>
2023-12-05 12:56:59 +0100

    Added javadocs to AutoSave

--- src/thread/AutoSave.java
+++ src/thread/AutoSave.java
@@ -5,8 +5,16 @@ public class AutoSave extends Thread {
     private final int sleepTime;
     private boolean shouldStop = false;
 
+    /**
+     * Constructor for AutoSave
+     * <p>
+     *     Kaster en IllegalArgumentException hvis sleepTime er mindre end 0
+     *     <p>
+     *         For at stoppe dette skal man kalde Interrupt()
+     * @param sleepTime Tiden mellem hver autosave i minutter
+     */
     public AutoSave(int sleepTime) {
-        this.sleepTime = sleepTime;
+        this.sleepTime = sleepTime*1000;
     }
 
     @Override
