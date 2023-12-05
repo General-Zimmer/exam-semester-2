2de7c382f1a94b15ec077081af6222e2d188362d
GZimmer <kristianz1000@gmail.com>
2023-12-05 12:26:30 +0100

    Correct indentation

--- src/thread/AutoSave.java
+++ src/thread/AutoSave.java
@@ -2,21 +2,21 @@ package thread;
 
 public class AutoSave extends Thread {
 
-        private final int sleepTime;
+    private final int sleepTime;
 
-        public AutoSave(int sleepTime) {
-            this.sleepTime = sleepTime;
-        }
+    public AutoSave(int sleepTime) {
+        this.sleepTime = sleepTime;
+    }
 
-        @Override
-        public void run() {
-            while (true) {
-                try {
-                    this.wait(sleepTime);
-                    System.out.println("AutoSave");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                this.wait(sleepTime);
+                System.out.println("AutoSave");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
+    }
 }
