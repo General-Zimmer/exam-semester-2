a609c940e380ba559b54544fe381a0d0d56065d4
nikolai <nikolaijuhl@gmail.com>
2023-12-06 11:02:27 +0100

    createWhisky test

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -4,6 +4,8 @@ import controller.Controller;
 import javafx.application.Application;
 import storage.Storage;
 
+import java.util.TimerTask;
+
 public class Main {
     public static void main(String[] args) {
         Controller.setStorage(new Storage());
