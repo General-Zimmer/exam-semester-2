f95984fe39a6e0baba83227b2846e1321126e44d
GZimmer <kristianz1000@gmail.com>
2023-12-07 10:35:36 +0100

    Removed redundant test

--- test/tests/FyldTests.java
+++ test/tests/FyldTests.java
@@ -13,28 +13,6 @@ import java.util.UUID;
 @SuppressWarnings("SpellCheckingInspection")
 public class FyldTests {
 
-
-    @Test
-    public void beregnOplaringsTest() {
-        // Basis data
-        String kornsort = "Lars' korn";
-        String destillering = "Destillering";
-        String kommentar = "Test123";
-        float mængde = 100;
-        LocalDate baseDate = LocalDate.now().minusDays(40);
-        LocalDate baseDate2 = LocalDate.now().minusDays(20);
-        Destillat destillat = new Destillat(UUID.randomUUID(), 1, kornsort, mængde, destillering, 48, baseDate, kommentar);
-        Destillat destillat2 = new Destillat(UUID.randomUUID(), 1, kornsort, mængde, destillering, 48, baseDate2, kommentar);
-        LocalDate fyldDato = LocalDate.now().minusDays(10);
-        Fyld fyld = new Fyld(fyldDato, "Søren");
-
-        fyld.addDestillat(destillat, mængde);
-        fyld.addDestillat(destillat2, mængde);
-
-        Assertions.assertEquals(10, fyld.beregnOplaringstid());
-
-    }
-
     @Test
     public void addDestillatTest() {
 
