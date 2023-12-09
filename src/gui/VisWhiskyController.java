839ba2dd6b2e8c31ae409b11cded96a877e74818
Secondo92 <secondo92@hotmail.com>
2023-12-09 18:01:57 +0100

    fjernet et par mellemrum lol

--- src/gui/VisWhiskyController.java
+++ src/gui/VisWhiskyController.java
@@ -33,27 +33,26 @@ public class VisWhiskyController  implements IStorageObserver {
     public void update() {
         Gui gui = Gui.getInstance();
         lwWhiskys.getItems().clear();
-        if (lager == null) {
+        if(lager == null) {
             return;
         }
 
         Set<Whisky> whiskys = new HashSet<>();
 
-        for (int i = 0; i < lager.getReoler().length; i++) {
-            for (int j = 0; j < lager.getReoler()[0].length; j++) {
+        for(int i = 0; i < lager.getReoler().length; i++) {
+            for(int j = 0; j < lager.getReoler()[0].length; j++) {
                 Fad currentFad = lager.getFad(i, j);
-                if (currentFad != null) {
+                if(currentFad != null) {
                     FadIndhold currentIndhold = currentFad.getBlanding();
-                    if (currentIndhold != null) {
+                    if(currentIndhold != null) {
                         Set<Whisky> whiskySets = currentIndhold.getWhiskyPåFyld();
-                        for (Whisky currentWhisky : whiskySets) {
+                        for(Whisky currentWhisky : whiskySets) {
                             whiskys.add(currentWhisky);
                         }
                     }
                 }
             }
         }
-
         lwWhiskys.getItems().addAll(whiskys);
     }
     public void setLager(Lager lager){
